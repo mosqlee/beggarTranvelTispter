ef9ccc2d51664fabf1fee2f3ee562c77e5f01fc3
wudan <wd_Wu@87money.com>
2017-12-18 10:43:13 +0800

    存档

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 import Button from 'material-ui/Button';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+const theme = createMuiTheme({
+    overrides: {
+      MuiButton: {
+        // Name of the styleSheet
+        root: {
+          // Name of the rule
+          background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+          borderRadius: 3,
+          border: 0,
+          color: 'white',
+          height: 48,
+          padding: '0 30px',
+          boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .30)',
+        },
+      },
+    },
+  });
+
+
 export default class Home extends Component {
     constructor(props) {
         super(props);
@@ -20,7 +40,9 @@ export default class Home extends Component {
             <div>
                 this is home~<br />
                 记一下数：{this.state.count}<br />
-                <Button raised onClick={() => this._handleClick()}>增</Button>
+                <MuiThemeProvider theme={theme}>
+                <Button  onClick={() => this._handleClick()}>增</Button>
+                </MuiThemeProvider>
             </div>
         )
     }
