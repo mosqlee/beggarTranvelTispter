de6cc7329e61027dec9abfa53c03a4909fdc9751
mosqlee <liweiwen92@163.com>
2017-12-19 17:34:59 +0800

    uadd travellist

---
+++ src/pages/TravelList/TravelList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import style from './TravelList.css';
+
+
+export default class TravelList extends Component {
+    render() {
+        return (
+            <div className={style.box}>
+                this is TravelList~
+            </div>
+        )
+    }
+}
--- src/router/router.js
+++ src/router/router.js
@@ -10,7 +10,7 @@ import Page1 from 'bundle-loader?lazy&name=page1!pages/Page1/Page1';
 import Counter from 'bundle-loader?lazy&name=counter!pages/Counter/Counter';
 import UserInfo from 'bundle-loader?lazy&name=userInfo!pages/UserInfo/UserInfo';
 import NotFound from 'bundle-loader?lazy&name=notFound!pages/NotFound/NotFound';
-
+import TravelList from 'bundle-loader?lazy&name=travelList!pages/TravelList/TravelList';
 const createComponent = (component) => () => (
     <Bundle load={component}>
         {
@@ -23,6 +23,8 @@ export default () => (
     <div>
         <Switch>
             <Route exact path="/" component={createComponent(Home)} />
+            <Route path="/userinfo" component={createComponent(UserInfo)} />
+            <Route path="/travelList" component={createComponent(TravelList)} />
             <Route component={createComponent(NotFound)} />
         </Switch>
     </div>
