f4e19cda18ebbd2e895daf60321b8de4c07ed6c8
wudan <wd_Wu@87money.com>
2017-12-27 17:57:42 +0800

    存档

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-
+import ReactDOM from 'react-dom';
 //样式插件
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
@@ -35,11 +35,20 @@ imageDatas = (function genImageUrl(imageDatasArr){
     return imageDatasArr;
   }
 )(imageDatas)
+//获取区间内的一个随机值
+function getRangeRandom(low,high){
+  return Math.ceil(Math.random() * (high - low) + low);
+}
 
 let ImgFigure = React.createClass({
   render:function(){
+    let styleObj = {};
+    //如果props属性中指定了这张图片的位置，则使用
+    if(this.props.arrange.pos){
+      styleObj = this.props.arrange.pos;
+    }
     return(
-      <figure className={styleSCSS.ImgFigure}>
+      <figure className={styleSCSS.ImgFigure} style={styleObj}>
         <img src={this.props.data.imageUrl} alt={this.props.data.title}/>
         <figcaption>
           <h2 className={styleSCSS.ImgTitle}>{this.props.data.title}</h2>
@@ -49,7 +58,166 @@ let ImgFigure = React.createClass({
   }
 })
 
+
+
+let GalleryByReactApp = React.createClass({
+  Constant:{
+    centerPos:{
+      left:0,
+      right:0
+    },
+    hPosRange:{//水平方向的取值范围
+      leftSecX:[0,0], 
+      rightSecX:[0,0],
+      y:[0,0] 
+    },
+    vPosRange:{//垂直方向的取值范围
+      topY:[0,0], 
+      x:[0,0] 
+    },
+  },
+
+  /**
+   * 重新布局所有图片
+   * 
+   */
+  rearrange : function(centerIndex){
+    let imgsArrangeArr = this.state.imgsArrangeArr,
+    Constant = this.Constant,
+    centerPos = Constant.centerPos,
+    hPosRange = Constant.hPosRange,    
+    vPosRange = Constant.vPosRange,
+    hPosRangeLeftSecX = hPosRange.leftSecX,
+    hPosRangeRightSecX = hPosRange.rightSecX,
+    hPosRangeY = hPosRange.y,
+    vPosRangeTopY = vPosRange.topY,
+    vPosRangeX = vPosRange.x,
+
+    imgsArrangeTopArr = [],
+    topImgNum = Math.ceil(Math.random() * 2), //取一个或者不取
+    topImgSpliceIndex = 0,
+    imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex,1);
+
+    //首先居中centerIndex的图片
+    imgsArrangeCenterArr[0].pos = centerPos;
+
+    //取出要布局上侧的图片的状态信息
+    topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
+    imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex,topImgNum);
+
+    //布局位于上侧的图片
+    imgsArrangeTopArr.forEach(function(value,index){
+      imgsArrangeTopArr[index].pos = {
+        top:getRangeRandom( vPosRangeTopY[0],vPosRangeTopY[1]),
+        left:getRangeRandom( vPosRangeX[0],vPosRangeX[1])
+      };
+    });
+
+    //布局左右两侧的图片
+    for(let i = 0,j = imgsArrangeArr.length,k=j/2;i<j;i++){
+      let hPosRangeLORX = null;
+      //前半部分布局左边，右半部分布局右边
+      if(i<k){
+        hPosRangeLORX = hPosRangeLeftSecX;
+      }else{
+        hPosRangeLORX = hPosRangeRightSecX;
+      }
+      imgsArrangeArr[i].pos = {
+        top:getRangeRandom(hPosRangeY[0],hPosRangeY[1]),
+        left:getRangeRandom(hPosRangeLORX[0],hPosRangeLORX[1]),
+      }
+    }
+    if(imgsArrangeTopArr && imgsArrangeTopArr[0]){
+      imgsArrangeArr.splice(topImgSpliceIndex,0,imgsArrangeTopArr[0]);
+    }
+    imgsArrangeArr.splice(centerIndex,0,imgsArrangeCenterArr[0]);
+
+    this.setState({
+      imgsArrangeArr:imgsArrangeArr
+    })
+  },
+  getInitialState : function(){
+    return{
+      imgsArrangeArr : [
+        // {
+        //   pos:{
+        //     left:'0',
+        //     top:'0'
+        //   }
+        // }
+      ]
+    };
+  },
+
+  //组件加载以后，为每张图片计算其位置的范围
+  componentDidMount : function(){
+    //首先拿到舞台的大小
+    let stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+    stageW = stageDOM.scrollWidth,
+    stageH = stageDOM.scrollHeight,
+    halfStageW = Math.ceil(stageW/2),
+    halfStageH = Math.ceil(stageH/2);
+
+    //拿到一个imgFigure的大小
+    let imgFigureDOM = ReactDOM.findDOMNode(this.refs.imgFigure0),
+    imgW = imgFigureDOM.scrollWidth,
+    imgH = imgFigureDOM.scrollHeight,
+    halfImgW = Math.ceil(imgW/2),
+    halfImgH = Math.ceil(imgH/2);
+
+    //计算中心图片的位置点
+    this.Constant.centerPos = {
+      left: halfStageW - halfImgW,
+      top: halfStageH - halfImgH
+    }
+
+    //计算左侧右侧区域图片排布位置的取值范围
+    this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+    this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW*3;
+    this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+    this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+    this.Constant.hPosRange.y[0] = -halfImgH;
+    this.Constant.hPosRange.y[1] = stageH - halfImgH;
+
+    //计算上侧区域图片排布位置的取值范围 
+    this.Constant.vPosRange.topY[0] = -halfImgH;
+    this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+    this.Constant.vPosRange.x[0] = halfImgW - imgW;
+    this.Constant.vPosRange.x[1] = halfImgW;
+
+    this.rearrange(0);
+
+  },
+  render:function(){
+    let controllerUnits = [],
+    imgFigures = [];
+    imageDatas.forEach(function(value,index) {
+      if(!this.state.imgsArrangeArr[index]){
+        this.state.imgsArrangeArr[index] = {
+          pos:{
+            left:0,
+            top:0
+          }
+        }
+      }
+      imgFigures.push(<ImgFigure data={value} ref={'imgFigure'+index} key={index} arrange={this.state.imgsArrangeArr[index]}/>);
+    }.bind(this));
+    return(
+      <section className = {styleSCSS.stage} ref="stage">
+        <section className = {styleSCSS.img}>
+        {imgFigures}
+        </section>
+        <nav className = {styleSCSS.controllerNav}>
+        {controllerUnits}
+        </nav>
+        </section>
+    )
+  }
+})
+
   class MenuAppBar extends React.Component {
+
+   
     state = {
       auth: true,
       anchorEl: null,
@@ -72,22 +240,11 @@ let ImgFigure = React.createClass({
       const { auth, anchorEl } = this.state;
       const open = Boolean(anchorEl);
       const bull = <span className={classes.bullet}>•</span>;
-      let controllerUnits = [],
-          imgFigures = [];
-          imageDatas.forEach(function(value) {
-            imgFigures.push(<ImgFigure data={value} />);
-          });
+
 
       return (
         <div className={classes.root}>
-        <section className = {styleSCSS.stage}>
-        <section className = {styleSCSS.img}>
-        {imgFigures}
-        </section>
-        <nav className = {styleSCSS.controllerNav}>
-        {controllerUnits}
-        </nav>
-        </section>
+       <GalleryByReactApp />
 
 
 
